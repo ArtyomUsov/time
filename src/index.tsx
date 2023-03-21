a6c92a068107cb778689ced5e920ac5b057b995d
Usov_Artem <artemka063@gmail.com>
2023-03-21 16:27:35 +0400

    clearing

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function tick() {
+  const element = (
+    <div className='body'>
+      <div className='main'>
+        <div>
+          <h1>Local Time</h1>
+          <h2 >{new Date().toLocaleTimeString()}.</h2>
+        </div>
+      </div>
+    </div>
+  );
+  root.render(element);
+}
+
+setInterval(tick, 1000);
